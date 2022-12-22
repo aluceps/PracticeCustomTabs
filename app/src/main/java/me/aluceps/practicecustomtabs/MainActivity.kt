5634e751ace0c45a88fa67083bc46a9ccb44e040
aluceps <aluceps@gmail.com>
2022-12-22 15:48:38 +0900

    入力したテキストを取得する

--- app/src/main/java/me/aluceps/practicecustomtabs/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicecustomtabs/MainActivity.kt
@@ -1,7 +1,7 @@
 package me.aluceps.practicecustomtabs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import me.aluceps.practicecustomtabs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +9,17 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.action.setOnClickListener {
+            val url = binding.textUrl.text?.toString()
+            if (url.isNullOrEmpty()) return@setOnClickListener
+        }
     }
 }
